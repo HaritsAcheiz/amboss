52389ae75bb665d8c51d0e853f72ca1c111cbaf4
Muhammad Harits R <harits.muhammad.only@gmail.com>
2023-05-26 01:38:26 +0700

    Done, just need to improve

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import csv
 import json
+import os
+import time
 
+from dataclasses import dataclass, asdict
 import httpx
 import selectolax
-from dataclasses import dataclass
 
 from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys, ActionChains
@@ -14,12 +17,19 @@ from selenium.webdriver.support import expected_conditions as ec
 import creds
 
 @dataclass
+class Item:
+    title: str
+    content: str
+    link_img: str
+    glossaries: str
+
+@dataclass
 class Scraper:
 
     def get_data(self):
         useragent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/113.0'
         ff_opt = Options()
-        # ff_opt.add_argument('-headless')
+        ff_opt.add_argument('-headless')
         ff_opt.add_argument('--no-sanbox')
         ff_opt.set_preference("general.useragent.override", useragent)
         ff_opt.page_load_strategy = 'eager'
@@ -49,25 +59,24 @@ class Scraper:
 
         title = driver.find_element(By.CSS_SELECTOR, 'article[data-e2e-test-id="learningCardContent"] > div:nth-of-type(3) > section > div.headerContainer--rfIL2 > div > h2').text
         content = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'article[data-e2e-test-id="learningCardContent"] > div:nth-of-type(3) > section > div[data-e2e-test-id="section-content-is-shown"] > div > div > div'))).text
-        link_image = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'article[data-e2e-test-id="learningCardContent"] > div:nth-of-type(3) > section > div[data-e2e-test-id="section-content-is-shown"] > div > div > div:nth-of-type(2)'))).get_attribute('data-source')
+        link_image = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'article[data-e2e-test-id="learningCardContent"] > div:nth-of-type(3) > section > div[data-e2e-test-id="section-content-is-shown"] > div > div > div:nth-of-type(2) > span'))).get_attribute('data-source')
         glossaries = []
         mouse_gloss_locs = (driver.find_elements(By.CSS_SELECTOR, f'div[data-e2e-test-id="section-content-is-shown"] > div > div > div > span.api.explanation'))
-        for mouse_gloss_loc in mouse_gloss_locs:
+        beacon = driver.find_element(By.CSS_SELECTOR, 'h1[data-e2e-test-id="articleHeader"')
+        for i, mouse_gloss_loc in enumerate(mouse_gloss_locs,1):
             try:
                 ActionChains(driver).move_to_element(mouse_gloss_loc).perform()
-                glossary = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'aside[data-e2e-test-id="tooltip-container"] > div:nth-of-type(2) > div > div'))).text
-                glossaries.append(glossary)
+                time.sleep(1)
+                glossary = wait.until(ec.presence_of_element_located((By.CSS_SELECTOR, 'aside[data-e2e-test-id="tooltip-container"] > div:nth-of-type(2) > div > div:nth-of-type(1)'))).text
+                glossaries.append(f"{i}. {glossary}")
+                ActionChains(driver).move_to_element(beacon).perform()
             except Exception as e:
                 print(e)
                 break
-        glossaries = ' \n'.join(glossaries)
-        print(title)
-        print(content)
-        print(link_image)
-        print(glossaries)
-
-        # driver.close()
-        # return cookies
+        glossaries = ' ;'.join(glossaries)
+        result = Item(title=title, content=content, link_img=link_image, glossaries=glossaries)
+        driver.close()
+        return asdict(result)
 
     def fetch(self, cookies):
         cookie_jar = dict()
@@ -102,10 +111,28 @@ class Scraper:
             payload4 = [{"operationName":"currentUserCourseBundles","variables":{"courseEid":"NS0--2"},"query":"query currentUserCourseBundles($courseEid: ID!) {\n  currentUserCourseBundles(courseEid: $courseEid) {\n    bundle {\n      eid\n      __typename\n    }\n    __typename\n  }\n}\n"},{"operationName":"currentUserCourseBundles","variables":{"eids":["hVac8j"]},"query":"query currentUserCourseBundles($eids: [ID!]!) {\n  currentUserBundles(eids: $eids) {\n    bundle {\n      eid\n      title\n      subtitle\n      articleEids\n      isCertified\n      targets {\n        eid\n        articleEid\n        articleTitle\n        anchorEid\n        anchorTitle\n        __typename\n      }\n      sessionTemplate {\n        eid\n        defaultMode\n        questionCount\n        availableQuestionCount\n        title\n        __typename\n      }\n      course {\n        eid\n        category\n        certificateInformation {\n          certificateType\n          percentToPass\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    userBundleDestinations {\n      status\n      bundleDestination {\n        eid\n        __typename\n      }\n      __typename\n    }\n    accessedAt\n    sessionEid\n    __typename\n  }\n}\n"},{"operationName":"articleQuestionResultsQuery","variables":{"eids":["bM0HMg"]},"query":"query articleQuestionResultsQuery($eids: [ID!]!) {\n  currentUserArticles(eids: $eids) {\n    article {\n      eid\n      __typename\n    }\n    questionResults {\n      sessionEid\n      questionEid\n      isAnswerCorrect\n      __typename\n    }\n    __typename\n  }\n}\n"},{"operationName":"userParticlesQuery","variables":{"eids":["bM0HMg"]},"query":"query userParticlesQuery($eids: [ID!]!) {\n  currentUserArticles(eids: $eids) {\n    article {\n      eid\n      __typename\n    }\n    userParticles {\n      particleEid\n      extension {\n        text\n        eid\n        ownerName\n        __typename\n      }\n      sharedExtensions {\n        text\n        eid\n        ownerName\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n"},{"operationName":"articleQuery","variables":{"eids":["bM0HMg"]},"query":"query articleQuery($eids: [ID!]!) {\n  currentUserArticles(eids: $eids) {\n    editorialLink {\n      ... on EditorialUrl {\n        url\n        __typename\n      }\n      ... on ForbiddenLink {\n        isForbidden\n        __typename\n      }\n      __typename\n    }\n    studyQuestionProgress {\n      studyObjectiveLabel\n      totalQuestions\n      __typename\n    }\n    article {\n      ...articleFullFragment\n      __typename\n    }\n    isLearned\n    __typename\n  }\n  maxQuestionsWithObjective: questionSessionCustomSize(\n    criteria: {learningCard: $eids, onlyStudyObjectiveRelated: true}\n  )\n  maxQuestionWithoutObjective: questionSessionCustomSize(\n    criteria: {learningCard: $eids}\n  )\n}\n\nfragment articleFullFragment on Article {\n  __typename\n  eid\n  title\n  synonyms\n  abstract\n  knowledgeCategory\n  stages\n  knowledgeScope\n  titleAnchor\n  patientNoteEid\n  updatedDate\n  tipsAndLinks {\n    description\n    url\n    additional\n    __typename\n  }\n  content {\n    ...particleFields\n    __typename\n  }\n  references {\n    ... on ArticleReference {\n      eid\n      __typename\n    }\n    ... on AnthologyReference {\n      eid\n      __typename\n    }\n    ... on BookReference {\n      eid\n      __typename\n    }\n    ... on UrlReference {\n      eid\n      __typename\n    }\n    ... on WebMdReference {\n      eid\n      __typename\n    }\n    ... on MiscReference {\n      eid\n      __typename\n    }\n    ... on UpToDateReference {\n      eid\n      __typename\n    }\n    __typename\n  }\n}\n\nfragment particleFields on Particle {\n  __typename\n  eid\n  partType\n  textType\n  globalStyles\n  title\n  titleReferenceMarkup\n  abstract\n  content\n  isSubParticle\n  titleAnchors\n  media {\n    ...mediaFragment\n    __typename\n  }\n}\n\nfragment mediaFragment on MediaAsset {\n  __typename\n  eid\n  title\n  editorialLink {\n    __typename\n    ... on EditorialUrl {\n      url\n      __typename\n    }\n    ... on ForbiddenLink {\n      isForbidden\n      __typename\n    }\n  }\n  description\n  canonicalUrl\n  aspectRatio\n  overlayUrl\n  externalAddition {\n    __typename\n    ... on ExternalAddition {\n      type\n      url\n      fallbackUrl\n      __typename\n    }\n    ... on BlockedExternalAddition {\n      type\n      __typename\n    }\n  }\n  multilayerAsset\n  copyright {\n    __typename\n    eid\n    html\n  }\n}\n"}]
             response = client.post('https://www.amboss.com/de/api/graphql', data=json.dumps(payload3), headers=headers2)
 
+
         print(response.json())
 
+    def to_csv(self, datas, filename):
+        headers = ['title', 'content', 'link_img', 'glossaries']
+        try:
+            file_exists = os.path.isfile(filename)
+            with open(filename, 'a', encoding='utf-16') as f:
+                writer = csv.DictWriter(f, delimiter=',', lineterminator='\n', fieldnames=headers)
+                if not file_exists:
+                    writer.writeheader()
+                if datas != None:
+                    writer.writerow(datas)
+                else:
+                    pass
+        except Exception as e:
+            print(e)
+            pass
+
     def main(self):
-        self.get_data()
+        data = self.get_data()
+        self.to_csv(data, 'result.csv')
         # self.fetch(cookies)
 
 if __name__ == '__main__':
